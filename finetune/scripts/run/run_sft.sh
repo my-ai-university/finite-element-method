f7b441aecd2d53ee3386bd44f30187c98323697b
UpUp Ashton Wang <upup.ashton.wang@gmail.com>
2025-03-08 16:36:56 -0800

    formalize launching script

--- finetune/scripts/run/run_sft.sh
+++ finetune/scripts/run/run_sft.sh
@@ -10,20 +10,18 @@ source ./finetune/scripts/set/env/set_vars.sh
 
 GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
-MODEL_VERSION=TOMMI-book-qa-test
+MODEL_VERSION=TOMMI-0.2-auto
 
-PY_SCRIPT=./finetune/sft/sft_trl.py # an advanced and more efficient version
-PY_CONFIG=./finetune/recipes/trl/"${MODEL_VERSION}".yaml
+PY_SCRIPT=./finetune/sft/sft_trl.py # the correct version for fine-tuning with instruct model
+#PY_SCRIPT=./finetune/sft/sft_navie.py # the inner core of hpo legacy, which should not be used anymore
 
-# PY_SCRIPT=./finetune/sft/sft_plain.py # the inner core of hpo
-# PY_CONFIG=./finetune/recipes/"${MODEL_VERSION}".yaml
-
-ACCEL_CONFIG=./finetune/recipes/accelerate_ds_cfgs/ds_zero3_carc.yaml
+PY_CONFIG=./finetune/recipes/"${MODEL_VERSION}".yaml
 
 echo ""
 echo "Accelerating script: ${PY_SCRIPT} with config: ${PY_CONFIG}"
 echo ""
 
+ACCEL_CONFIG=./finetune/recipes/accelerate_ds_cfgs/ds_zero2_carc.yaml
 ACCELERATE_LOG_LEVEL=info accelerate launch \
     --num_processes="${GPU_COUNT}" \
     --config_file "${ACCEL_CONFIG}" "${PY_SCRIPT}" --config "${PY_CONFIG}"
